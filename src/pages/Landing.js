d52fe25bdfb0393b8c229105ec00f6cd2ed9c819
Kristiyan Strahilov <55084258+kpuc00@users.noreply.github.com>
2022-06-09 01:53:36 +0200

    height fix

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -5,17 +5,15 @@ import { Footer } from "../components";
 
 const Landing = () => {
   return (
-    <Box display="flex" flexDirection="column" minHeight="100vh">
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        minHeight="70vh"
-      >
-        <Typography variant="h3" gutterBottom component="div">
-          Coming soon!
-        </Typography>
-      </Box>
+    <Box
+      display="flex"
+      flexDirection="column"
+      minHeight="100vh"
+      textAlign="center"
+    >
+      <Typography variant="h3" gutterBottom component="div" sx={{ mt: 25 }}>
+        Coming soon!
+      </Typography>
 
       <Footer />
     </Box>
